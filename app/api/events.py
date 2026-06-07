1a0d7a959dba481afde2f3ef0907d0a595099dae
Fernando De Santiago <fdesantiago@miners.utep.edu>
2026-06-07 11:54:10 -0600

    refactor: move event creation from API to services

--- app/api/events.py
+++ app/api/events.py
@@ -1,18 +1,10 @@
 from fastapi import APIRouter
-from models.events import Event,EventType
+from services.event_service import get_events
 
 router = APIRouter()
 
 @router.get("/events")
 
-def get_event():
-    event_1 = Event(event_id=1,event_type=EventType.LOGIN_FAILED,source="SERV-01",message="Failed login detected")
-
-    event_2 = Event(event_id=2,event_type=EventType.LOGIN_SUCCESS,source="SERV-01",message="Login successful")
-
-    event_3 = Event(event_id=3,event_type=EventType.SYSTEM_DOWN,source="SERV-01",message="System down check if powered on")
-   
-
-    list_of_events=[event_1,event_2,event_3]
-
-    return list_of_events
+def get_events():
+    events=get_events()
+    return events
