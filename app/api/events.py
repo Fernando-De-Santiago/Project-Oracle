ee2861c08aae18ad6b56f4e7894328db2765e3bc
Fernando De Santiago <fdesantiago@miners.utep.edu>
2026-06-07 12:43:43 -0600

    feat: add event retrieval by id with 404 handling

--- app/api/events.py
+++ app/api/events.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter
-from services.event_service import get_events
+from fastapi import APIRouter, HTTPException
+from services.event_service import get_events, search_event_by_id
 
 router = APIRouter()
 
@@ -8,3 +8,12 @@ router = APIRouter()
 def get_events_route():
     events=get_events()
     return events
+
+@router.get("/events/{id}")
+
+def get_event_by_id(id: int):
+    
+    event = search_event_by_id(id)
+    if event == None:
+        raise HTTPException(status_code=404,detail="Event not found")
+    return event
--- app/services/event_service.py
+++ app/services/event_service.py
@@ -8,7 +8,7 @@ def get_events():
     event_3 = Event(event_id=3,event_type=EventType.SYSTEM_DOWN,source="SERV-01",message="System down check if powered on")
     return[event_1,event_2,event_3]
 
-def get_event_by_id(id):
+def search_event_by_id(id: int):
     events=get_events()
     for i in events:
         if i.event_id==id:
