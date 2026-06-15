dfd902d00484cd512a9cd29299f19e10f93e3b70
Fernando De Santiago <fdesantiago@miners.utep.edu>
2026-06-15 11:47:26 -0600

    feat(auth): implement user schema design including db model and api request/response models

---
+++ app/models/api/users.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, EmailStr, Field
+from datetime import datetime
+
+class UserRegister(BaseModel):
+    email : EmailStr
+    username : str = Field(min_length=10, max_length=20)
+    password: str = Field(min_length=12)
+
+class UserLogin(BaseModel):
+    email : EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    user_id: int
+    username : str
+    email: EmailStr
+    created_at: datetime
