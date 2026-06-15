4f5b1fede319e1ae65cc582c62728127a18af580
Fernando De Santiago <fdesantiago@miners.utep.edu>
2026-06-15 12:55:50 -0600

    feat(auth): complete user registration flow with validation, hashing, and endpoint

---
+++ app/api/user.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter, HTTPException, status, Depends
+from services.user_event import create_user, authenticate_user
+from models.api.users import UserRegister, UserLogin, UserResponse
+from sqlalchemy.orm import Session
+from sqlalchemy import select
+from db.session import get_db
+from models.db.user import Users
+
+router = APIRouter()
+
+@router.post("/users",response_model=UserResponse, status_code=status.HTTP_201_CREATED)
+
+def create_user_route(user: UserRegister, db: Session = Depends(get_db)):
+    success, new_user, message= create_user(db,user)
+    if not success:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,detail=message)
+    return new_user
--- app/services/user_event.py
+++ app/services/user_event.py
@@ -6,14 +6,21 @@ from models.db.user import Users
 from bcrypt import hashpw, gensalt, checkpw
 
 def create_user(db: Session, user_data:UserRegister):
+    check_email = db.query(Users).filter(Users.email == user_data.email).first()
+    if check_email:
+        return False, None, "Email already in use"
+    check_user = db.query(Users).filter(Users.username == user_data.username).first()
+    if check_user:
+        return False, None, "Username already in use"
     password = user_data.password.encode('utf-8')
     salt = gensalt()
     password_hashed=hashpw(password, salt).decode('utf-8')
     new_user=Users(username=user_data.username, email=user_data.email, password_hash=password_hashed)
+    
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    return new_user
+    return True,new_user,"Created New User"
 
 def authenticate_user(db: Session, user_data:UserLogin):
     user = db.query(Users).filter(Users.email == user_data.email).first()
