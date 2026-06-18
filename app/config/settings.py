16175691b48e125fd38df7278490406bf67933cb
Fernando De Santiago <fdesantiago@miners.utep.edu>
2026-06-17 19:40:04 -0600

    feat(config): add application settings using Pydantic BaseSettings

---
+++ app/config/settings.py
@@ -0,0 +1,11 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    DATABASE_URL : str
+
+    JWT_SECRET_KEY :str
+    JWT_ALGORITHM :str
+    JWT_EXPIRE_MINUTES :int
+    class Config:
+        env_file = ".env"
