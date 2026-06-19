e107352e86d60ce09a3d812b06f1e83030a13d29
Fernando De Santiago <fdesantiago@miners.utep.edu>
2026-06-19 10:46:18 -0600

    refactor(config): standardize settings and improve environment configuration

--- app/config/settings.py
+++ app/config/settings.py
@@ -2,10 +2,15 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    DATABASE_URL : str
+    DATABASE_URL: str
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    JWT_EXPIRE_MINUTES: int
 
-    JWT_SECRET_KEY :str
-    JWT_ALGORITHM :str
-    JWT_EXPIRE_MINUTES :int
-    class Config:
-        env_file = ".env"
+    model_config = {
+        "env_file": ".env",
+        "extra": "forbid"
+    }
+
+
+settings = Settings()
