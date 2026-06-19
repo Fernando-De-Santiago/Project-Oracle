8872da5cb0011af6b7aea294d4f719a8158ecc2e
Fernando De Santiago <fdesantiago@miners.utep.edu>
2026-06-19 10:55:59 -0600

    refactor(models/db): clean up and standardize user and event models

--- app/models/db/event.py
+++ app/models/db/event.py
@@ -1,15 +1,21 @@
-from sqlalchemy import Column, Integer, String, DateTime, Enum
-from sqlalchemy.orm import declarative_base
-from datetime import datetime, UTC
+from sqlalchemy import Column, Integer, String, DateTime, Enum, func
 from models.core.enums import EventType
+from db.base import Base
 
-
-Base = declarative_base()
 
 class Events(Base):
     __tablename__ = "events"
-    event_id = Column(Integer, primary_key= True, index=True)
-    created_at = Column(DateTime, nullable=False)
-    event_type = Column(String(50),nullable= False)
-    source = Column(String, nullable= False)
-    message = Column(String,nullable= True)
+
+    event_id = Column(Integer, primary_key=True, index=True)
+
+    created_at = Column(
+        DateTime,
+        server_default=func.now(),
+        nullable=False
+    )
+
+    event_type = Column(Enum(EventType), nullable=False)
+
+    source = Column(String, nullable=False)
+
+    message = Column(String, nullable=True)
--- app/models/db/user.py
+++ app/models/db/user.py
@@ -1,14 +1,17 @@
 from sqlalchemy import Column, Integer, String, DateTime, Text
 from sqlalchemy.sql import func
-from sqlalchemy.orm import declarative_base
+from db.base import Base
 
-Base = declarative_base()
 
-class Users(Base):
+class User(Base):
     __tablename__ = "users"
 
     user_id = Column(Integer, primary_key=True, index=True)
-    username = Column(String(50), unique=True, nullable=False)
-    email = Column(String(100), unique=True, nullable=False)
+
+    username = Column(String(50), unique=True, index=True, nullable=False)
+
+    email = Column(String(100), unique=True, index=True, nullable=False)
+
     password_hash = Column(Text, nullable=False)
-    created_at = Column(DateTime, nullable=False, server_default=func.now())
+
+    created_at = Column(DateTime, server_default=func.now(), nullable=False)
